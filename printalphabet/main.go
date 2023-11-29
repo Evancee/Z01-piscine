22f2703be3712e15da42569de019bd2a5067f17f
Evans omondievans2002@gmail.com <omondievans2002@gmail.com>
2023-11-29 16:13:38 +0300

    my first go program

--- printalphabet/main.go
+++ printalphabet/main.go
@@ -1,22 +1,7 @@
 package main
 
-import (
-	"fmt"
-	"os"
-)
-
-func PrintRune(r rune) error {
-	if _, err := fmt.Printf("%c", r); err != nil {
-		return err
-	}
-	return nil
-}
-
 func main() {
-	for _, r := range "abcdefghijklmnopqrstuvwxyz" {
-		if err := PrintRune(r); err != nil {
-			fmt.Fprintf(os.Stderr, "Error printing rune: %v\n", err)
-			os.Exit(1)
-		}
-	}
+	alphabets := "abcdefghijklmnopqrstuvwxyz"
+
+	print(alphabets)
 }
