423ab1a5dc1f3c5ce90d894eb475e46f6b4efba4
Evans omondievans2002@gmail.com <omondievans2002@gmail.com>
2023-11-30 05:46:44 +0300

    my fifth program

---
+++ printcomb.go
@@ -0,0 +1,21 @@
+package piscine
+
+import "github.com/01-edu/z01"
+func printcomb() {
+	for x := '0' ; x <= '9' ; x++ {
+		for y := '1' y <= '9' ; y++ {
+			for z := '2' ; z <= '9' ; z++ {
+				if x < y && y < z {
+					z01.PrintRune(x)
+					z01.PrintRune(y)
+					z01.PrintRune(z)
+					if x != '7' || y != '8' || z != '9' {
+						z01.PrintRune(',')
+						z01.PrintRune(' ')
+					}
+				}
+			}
+		}
+	}
+    z01.PrintRune('\n')
+}
