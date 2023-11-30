2fcbf9ee1b383e98c742acfcbe51fc5d44250f0d
Evans omondievans2002@gmail.com <omondievans2002@gmail.com>
2023-11-30 05:53:14 +0300

    my fifth program

--- printcomb.go
+++ printcomb.go
@@ -4,7 +4,7 @@ import "github.com/01-edu/z01"
 
 func printcomb() {
 	for x := '0' ; x <= '9' ; x++ {
-		for y := '1' y <= '9' ; y++ {
+		for y := '1' ; y <= '9'; y++ {
 			for z := '2' ; z <= '9' ; z++ {
 				if x < y && y < z {
 					z01.PrintRune(x)
