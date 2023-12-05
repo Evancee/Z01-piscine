aa8945bd6611ba0044c34473d932cf9cf9fdc350
Evans omondievans2002@gmail.com <omondievans2002@gmail.com>
2023-12-05 15:44:11 +0300

    003

---
+++ iterativepower.go
@@ -0,0 +1,12 @@
+package piscine
+
+func IterativePower(nb int, power int) int {
+	if power == 0 {
+		return 1
+	}
+	result := nb
+	for i := 1; i < power; i++ {
+		result *= nb
+	}
+	return result
+}
