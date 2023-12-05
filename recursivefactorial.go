88db7c74e5bd18db255476df54c58814760b22f5
Evans omondievans2002@gmail.com <omondievans2002@gmail.com>
2023-12-05 15:25:26 +0300

    0022

--- recursivefactorial.go
+++ recursivefactorial.go
@@ -4,10 +4,8 @@ func RecursiveFactorial(nb int) int {
 	result := 1
 	if nb == 0 || nb == 1 {
 		return 1
-	} else if nb > 1 && nb < 23 {
-		for i := 1; i <= nb; i++ {
-			result = result * i
-		}
+	} else if nb > 1 && nb <= 23 {
+		result = nb * RecursiveFactorial(nb-1)
 		return result
 	} else {
 		return 0
