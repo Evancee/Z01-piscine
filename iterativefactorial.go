70c5a8e39e333f94acd54f35616279d474fa714b
Evans omondievans2002@gmail.com <omondievans2002@gmail.com>
2023-12-05 15:00:04 +0300

    quest4 001

--- iterativefactorial.go
+++ iterativefactorial.go
@@ -1,18 +1,15 @@
 package piscine
 
-import "github.com/01-edu/z01"
-
-func main() {
-	z01.PrintRune(IterativeFactorial(6))
-}
-
 func IterativeFactorial(nb int) int {
 	result := 1
 	if nb == 0 || nb == 1 {
 		return 1
+	} else if nb > 1 && nb < 23 {
+		for i := 1; i <= nb; i++ {
+			result = result * i
+		}
+		return result
+	} else {
+		return 0
 	}
-	for i := 1; i <= nb; i++ {
-		result = result * i
-	}
-	return result
 }
