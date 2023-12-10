7122a9914095a1ec6e3b74a57562de0b993c534f
Evans omondievans2002@gmail.com <omondievans2002@gmail.com>
2023-12-10 16:23:39 +0300

    strg

--- printstr.go
+++ printstr.go
@@ -3,8 +3,7 @@ package piscine
 import "github.com/01-edu/z01"
 
 func PrintStr(s string) {
-	for _, char := range Str {
-		z01.PrintRune("%c", char)
+	for _, char := range s {
+		z01.PrintRune(char)
 	}
-	z01.PrintRune(' ')
 }
