8594a7b9555bc0334cc7a6a2c4c93d7bfc054f2b
Evans omondievans2002@gmail.com <omondievans2002@gmail.com>
2023-12-11 15:46:46 +0300

    sqrr

--- sqrt.go
+++ sqrt.go
@@ -1,11 +1,10 @@
 package piscine
 
-import "gitbub.com/01-edu/z01"
-
 func Sqrt(nb int) int {
-	for i := 0 ; i*i <= nb i++ {
-      if i*i == nb{
-		return 1
-	  }
-	} return 0
+	for i := 0; i*i <= nb; i++ {
+		if i*i == nb {
+			return 1
+		}
+	}
+	return 0
 }
