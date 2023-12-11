59569b222918b0963c567210a08a82536e9d82a2
Evans omondievans2002@gmail.com <omondievans2002@gmail.com>
2023-12-11 15:32:43 +0300

    sqr

--- sqrt.go
+++ sqrt.go
@@ -1,14 +1,11 @@
 package piscine
 
-import "math"
+import "gitbub.com/01-edu/z01"
 
 func Sqrt(nb int) int {
-	if nb < 0 {
-		return 0
-	}
-	sqrt := math.Sqrt(float64(nb))
-	if math.Floor(sqrt) == sqrt {
-		return int(sqrt)
-	}
-	return 0
+	for i := 0 ; i*i <= nb i++ {
+      if i*i == nb{
+		return 1
+	  }
+	} return 0
 }
