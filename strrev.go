8015d72f5fc0a95e26692c5e4c0c6a52206fee3a
Evans omondievans2002@gmail.com <omondievans2002@gmail.com>
2023-12-11 09:43:35 +0300

    strev

--- strrev.go
+++ strrev.go
@@ -2,7 +2,7 @@ package piscine
 
 func StrRev(s string) string {
 	reversed := ""
-	for i := len(s) - 1; i >= 0; i++ {
+	for i := len(s) - 1; i >= 0; i-- {
 		reversed += string(s[i])
 	}
 	return reversed
