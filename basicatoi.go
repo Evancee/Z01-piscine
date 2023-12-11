fad083dcceaa4f1299f1e86f1df33f1096f552bc
Evans omondievans2002@gmail.com <omondievans2002@gmail.com>
2023-12-11 09:58:08 +0300

    atoi

---
+++ basicatoi.go
@@ -0,0 +1,12 @@
+package piscine
+
+func BasicAtoi(s string) int {
+	result := 0
+	for _, char:= range s {
+		if c<'0' || c>'9'
+		return 0 
+	}
+	digit := int(c - '0')
+	result = result*10 + digit
+}
+return result
