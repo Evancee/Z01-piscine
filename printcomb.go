c826fe6ecacf10aa7d8e05e84f7e8d21bebf9ee8
Evans omondievans2002@gmail.com <omondievans2002@gmail.com>
2023-12-11 16:00:56 +0300

    prntcmb

--- printcomb.go
+++ printcomb.go
@@ -3,10 +3,12 @@ package piscine
 import "github.com/01-edu/z01"
 
 func printcomb() {
-	for i := 0; i <= 7; i++ {
-		for j := i + 1; j <= 8; j++ {
-			for k := j + 1; k <= 9; k++ {
-				z01.PrintRune("%d%d%d, ", i, j, k)
+	for i := '0'; i <= '7'; i++ {
+		for j := i + '1'; j <= '8'; j++ {
+			for k := j + '1'; k <= '9'; k++ {
+				z01.PrintRune(i)
+				z01.PrintRune(j)
+				z01.PrintRune(k)
 			}
 		}
 	}
