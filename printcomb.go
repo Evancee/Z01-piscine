43c3c95f417f4258e8c0959b630c0d1fd04aa4a0
Evans omondievans2002@gmail.com <omondievans2002@gmail.com>
2023-12-13 13:01:36 +0300

    p

--- printcomb.go
+++ printcomb.go
@@ -2,7 +2,7 @@ package piscine
 
 import "github.com/01-edu/z01"
 
-func Printomb() {
+func PrintComb() {
 	for i := '0'; i <= '7'; i++ {
 		for j := i + '1'; j <= '8'; j++ {
 			for k := j + '2'; k <= '9'; k++ {
