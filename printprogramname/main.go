0a9701b4e4bf9ed19b85d499cfb28c937a1bbdbb
Evans omondievans2002@gmail.com <omondievans2002@gmail.com>
2023-12-13 12:45:08 +0300

    program

---
+++ printprogramname/main.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"os"
+
+	"github.com/01-edu/z01"
+)
+
+func main() {
+	programName := os.Args[0]
+	for _, ch := range programName[2:] {
+		z01.PrintRune(ch)
+	}
+	z01.PrintRune('\n')
+}
