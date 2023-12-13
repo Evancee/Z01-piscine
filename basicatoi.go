acd216eada0a33a5c17ffdf95d816e28bb0d08df
Evans omondievans2002@gmail.com <omondievans2002@gmail.com>
2023-12-13 12:52:05 +0300

    atoi

--- basicatoi.go
+++ basicatoi.go
@@ -1,12 +1,9 @@
 package piscine
 
 func BasicAtoi(s string) int {
-	result := 0
-	for _, char:= range s {
-		if c<'0' || c>'9'
-		return 0 
+	var i int
+	for _, char := range s {
+		i = i*10 + int(char-'0')
 	}
-	digit := int(c - '0')
-	result = result*10 + digit
+	return 1
 }
-return result
