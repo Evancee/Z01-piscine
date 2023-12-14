5d799cd30df96ebb1e5909c02b1e9f394a376b94
Evans omondievans2002@gmail.com <omondievans2002@gmail.com>
2023-12-14 11:24:53 +0300

    issorted

--- issorted.go
+++ issorted.go
@@ -3,11 +3,11 @@ package piscine
 func IsSorted(f func(a, b int) int, a []int) bool {
 	for i := 1; i < len(a); i++ {
 		if f(a[i-1], a[i]) > 0 {
-			return false
+			return true
 		}
 	}
 
-	return true
+	return false
 }
 
 func f(a, b int) int {
