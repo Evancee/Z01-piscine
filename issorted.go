190375252cad9c282785c451ee48894a5b8214d3
Evans omondievans2002@gmail.com <omondievans2002@gmail.com>
2023-12-14 12:14:52 +0300

    issorted

--- issorted.go
+++ issorted.go
@@ -12,9 +12,9 @@ func IsSorted(f func(a, b int) int, a []int) bool {
 
 func f(a, b int) int {
 	if a > b {
-		return -1
-	} else if a < b {
 		return 1
+	} else if a < b {
+		return -1
 	}
-	return 0
+	return 1
 }
