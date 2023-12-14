be76df3264d0c03213564692280692cf3ca123a2
Evans omondievans2002@gmail.com <omondievans2002@gmail.com>
2023-12-14 17:31:58 +0300

    makerange

--- makerange.go
+++ makerange.go
@@ -1,12 +1,13 @@
 package piscine
 
 func MakeRange(min, max int) []int {
-	if min >= max {
+	if max > min {
+		result := make([]int, max-min)
+		for i := min; i < max-min; i++ {
+			result[i] += min + i
+		}
+		return result
+	} else {
 		return nil
 	}
-	result := make([]int, max-min)
-	for i := min; i < max-min; i++ {
-		result[i] += min + i
-	}
-	return result
 }
