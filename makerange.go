1eaf4cc3509bd7edf41fcab324b189513e82f56f
Evans omondievans2002@gmail.com <omondievans2002@gmail.com>
2023-12-14 15:57:35 +0300

    makerng

--- makerange.go
+++ makerange.go
@@ -6,7 +6,7 @@ func MakeRange(min, max int) []int {
 	}
 	result := make([]int, max-min+1)
 	for i := max - min; i >= 0; i-- {
-		result[i] = max - 1 - i
+		result[i] = max - 1
 	}
 	return result
 }
