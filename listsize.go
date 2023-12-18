f3f2a9b696280242d1a018e050d880c7beabc61e
Evans omondievans2002@gmail.com <omondievans2002@gmail.com>
2023-12-18 17:33:33 +0300

    listsize

---
+++ listsize.go
@@ -0,0 +1,20 @@
+type NodeL struct {
+	Data interface{}
+	Next *NodeL
+}
+
+type List struct {
+	Head *NodeL
+	Tail *NodeL
+}
+
+func ListSize(l *List) int {
+	size := 0
+	iterate := l.Head
+	for iterate != nil {
+		size++
+		iterate = iterate.Next
+	}
+	return size
+
+}
