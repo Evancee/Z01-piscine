e0b658a3b12bcef49af7583b6928d8171af23780
Evans omondievans2002@gmail.com <omondievans2002@gmail.com>
2023-12-18 18:55:46 +0300

    nbr

--- printnbrinorder.go
+++ printnbrinorder.go
@@ -3,18 +3,24 @@ package piscine
 import "github.com/01-edu/z01"
 
 func PrintNbrInOrder(n int) {
-	if n < 0 {
-		return
-	}
+	if n <= 0 {
+		z01.PrintRune('0')
+	} else {
+		result := []int{}
+		for n > 0 {
+			result = append(result, n%10)
+			n /= 10
+		}
+		for i := 0; i < len(result); i++ {
+			for j := i + 1; j < len(result)-i-1; j++ {
+				if result[j] > result[j+1] {
+					result[j], result[j+1] = result[j+1], result[j]
+				}
+			}
+		}
+		for _, c := range result {
+			z01.PrintRune(rune(c + '0'))
+		}
 
-	for n > 0 {
-
-		digit := n % 10
-
-		runeDigit := rune(digit)
-
-		z01.PrintRune(runeDigit + '1')
-
-		n /= 10
 	}
 }
