9fd072df2cd851c61df13963d3bc41f55f326512
Evans omondievans2002@gmail.com <omondievans2002@gmail.com>
2023-12-19 16:14:19 +0300

    trimatoi

--- trimatoi.go
+++ trimatoi.go
@@ -1,22 +1,23 @@
 package piscine
 
 func TrimAtoi(s string) int {
+	if len(s) == 0 || s[0] == '-' && len(s) == 1 {
+		return 0
+	}
 	isNegative := false
-	number := 0
-
-	for _, char := range s {
-		if char == '-' {
-			isNegative = true
-		} else if char >= '0' && char <= '9' {
-			number = number*10 + int(char-'0')
-		} else {
-			return 0
+	if s[0] == '-' {
+		isNegative = true
+		s = s[1:]
+	}
+	num := 0
+	for _, v := range s {
+		if v < '0' || v > '9' {
+			break
 		}
+		num = num*10 + int(v) - 48
 	}
-
 	if isNegative {
-		number = -number
+		num *= -1
 	}
-
-	return number
+	return num
 }
